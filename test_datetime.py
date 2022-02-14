e38cec809b855fd50105c176eed271e95e69cdd6
Ciro Molina <cmolina@itgtec.com>
2022-02-13 22:00:47 -0500

    format update

--- test_datetime.py
+++ test_datetime.py
@@ -6,11 +6,11 @@ import numpy as np
 
 # get local now datetime with timezone
 time0 = datetime.now().astimezone()
-print('time0: ',time0)
+print('time0: ', time0)
 print(time0.tzname())
 
 # convert to numpy datetime64 (UTC)
-time0_utc = np.datetime64(time0,'ns')
+time0_utc = np.datetime64(time0, 'ns')
 print(time0_utc)
 print(np.int64(time0_utc))
 ##print(np.int64(np.datetime64(datetime.utcnow(), 'ns')))
@@ -19,13 +19,13 @@ print(np.int64(time0_utc))
 # local datetime
 time1 = datetime.strptime("2021-03-22 16:10", "%Y-%m-%d %H:%M").astimezone()
 ##time1 = time1.astimezone()
-print('time1: ',time1)
+print('time1: ', time1)
 
 # convert to numpy datetime64 (UTC)
-time1_utc = np.datetime64(time1,'ns')
+time1_utc = np.datetime64(time1, 'ns')
 print(time1_utc)
 
-#================================================================================
+# ================================================================================
 
 import pandas as pd
 
@@ -42,15 +42,14 @@ pdf['TS'] = pdf['TS'].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
 pdf['TS'] = pdf['TS'].str[:-3]
 
 # Check printed timestamp and write to csv to check as well, in both cases it should follow the stand format: 2017-08-22 13:25:28.875
-pdf.to_csv("C:\\Users\cmolina\\Desktop\\_MyFiles\\_SORBA files\\Task #1\\test_dsXXX.csv", sep=',', encoding='utf-8', index=False)
-
-
+pdf.to_csv("C:\\Users\cmolina\\Desktop\\_MyFiles\\_SORBA files\\Task #1\\test_dsXXX.csv", sep=',', encoding='utf-8',
+           index=False)
 
 # ==============================================================
 
 
 # get local now datetime with timezone
-time0_local = datetime.now() #.astimezone()
-print('time0_local: ',time0_local)
+time0_local = datetime.now()  # .astimezone()
+print('time0_local: ', time0_local)
 print(time0_local.tzname())
 print(time0_local.tzinfo)
